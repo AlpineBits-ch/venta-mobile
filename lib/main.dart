32638ad10b3fd4f8ca2ab4a9cb075b37b91ca170
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-31 12:50:20 +0200

    feat: add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'core/device/device_id_service.dart';
 import 'core/push/push_notification_service.dart';
 import 'core/realtime/realtime_service.dart';
 import 'features/auth/data/auth_repository.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,5 +35,18 @@ Future<void> main() async {
     unawaited(getIt<RealtimeService>().start());
     unawaited(startPushServices());
   }
-  runApp(const App());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://0de5da8d45ec13c46a2826e6bfbd0589@o4511596550946816.ingest.de.sentry.io/4511829505671248';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.enableLogs = true;
+
+    },
+    appRunner: () => runApp(SentryWidget(child: const App())),
+  );
+
 }
